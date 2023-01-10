cbf7c385d3e630288b8ff9a5f98c5ccea1c6ae00
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-10 18:32:47 -0300

    add ToLower() method

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Matrizes
     public class Matrizes
     {
         static void Main(string[] args)
-        {
+        { 
             //criacao de matriz e classifica-las
             string[] pallets = { "B14", "A11", "B12", "A13" };
 
@@ -20,6 +20,22 @@ namespace Matrizes
             Console.WriteLine("");
             Console.WriteLine("Reversed.....");
             Array.Reverse(pallets);
+            foreach(var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            } 
+
+            //Limpar itens
+            string[] pallets2 = { "B24", "A21", "B22", "A23" };
+            Console.WriteLine("");
+
+            Console.WriteLine($"Before: {pallets[0].ToLower()}");
+            Array.Clear(pallets, 0, 2);
+
+            if (pallets[0] != null)
+                Console.WriteLine($"After: {pallets[0].ToLower()}");
+
+            Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
             foreach(var pallet in pallets)
             {
                 Console.WriteLine($"-- {pallet}");
