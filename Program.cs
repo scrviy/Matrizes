7a47c8073f71e941661fc99aff85d9d2d9539193
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-10 18:05:16 -0300

    add sort and reverse method

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Matrizes
+{
+    public class Matrizes
+    {
+        static void Main(string[] args)
+        {
+            //criacao de matriz e classifica-las
+            string[] pallets = { "B14", "A11", "B12", "A13" };
+
+            Console.WriteLine("Sorted....");
+            Array.Sort(pallets);
+            foreach(var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+
+            //inverter a ordem dos paletes
+            Console.WriteLine("");
+            Console.WriteLine("Reversed.....");
+            Array.Reverse(pallets);
+            foreach(var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+        }
+    }
+}
